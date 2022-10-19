61422a60a0b4750b01f934f4904ceb655b83a9e0
adiko01 <dev@adiko01.de>
2022-10-19 21:54:29 +0200

    Changes in the config.yml - New desing of helptext - Manuel for the new wildcards - New Demo tex
    Changes in the EditString
    - Wildcards could now be spelled in small or CAPITAL letters
    - motd
    - max-players

--- src/main/java/de/adiko01/anothertablistplugin/tools/EditString.java
+++ src/main/java/de/adiko01/anothertablistplugin/tools/EditString.java
@@ -1,20 +1,27 @@
 package de.adiko01.anothertablistplugin.tools;
 
-import de.adiko01.anothertablistplugin.AnotherTablistPlugin;
-import de.adiko01.anothertablistplugin.tools.TimeTool;
 import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
 
 public class EditString {
 
     public static String withoutParam(String str) {
         return str
                 .replace("{players}", String.valueOf(Bukkit.getOnlinePlayers().size()))
+                .replace("{PLAYERS}", String.valueOf(Bukkit.getOnlinePlayers().size()))
+                .replace("{max-players}", String.valueOf(Bukkit.getMaxPlayers()))
+                .replace("{MAX-PLAYERS}", String.valueOf(Bukkit.getMaxPlayers()))
+                .replace("{motd}", String.valueOf(Bukkit.getMotd()))
+                .replace("{MOTD}", String.valueOf(Bukkit.getMotd()))
                 .replace("{time}", TimeTool.getTime())
+                .replace("{TIME}", TimeTool.getTime())
                 .replace("{time2}", TimeTool.getTime2())
+                .replace("{TIME2}", TimeTool.getTime2())
                 .replace("{date}", TimeTool.getDate())
+                .replace("{DATE}", TimeTool.getDate())
                 .replace("{weekday}", TimeTool.getWeekDay())
-                .replace("{month}", TimeTool.getMonth()
-        );
+                .replace("{WEEKDAY}", TimeTool.getWeekDay())
+                .replace("{month}", TimeTool.getMonth())
+                .replace("{Month}", TimeTool.getMonth()
+                );
     }
 }
